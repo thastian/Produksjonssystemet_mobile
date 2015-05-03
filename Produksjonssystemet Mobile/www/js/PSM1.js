1ac8b777f24671f57528b6771409d4f2e5baf65d
Stian Bluth Levik <stian.levik@lilleborg.no>
2015-05-03 18:35:13 +0200

    Initial commit

---
+++ Produksjonssystemet Mobile/www/js/PSM1.js
@@ -0,0 +1,37 @@
+/**
+ * Created by swlevi on 13.04.2015.
+ */
+
+function Pagechangetolillebloggen () {
+    $("#PageMainmenu").hide();
+    $("#PageLillebloggen").show();
+
+
+}
+
+function Pagechangetomainfromlillebloggen () {
+    $("#PageMainmenu").show();
+    $("#PageLillebloggen").hide();
+}
+
+function Pagechangetomainfromlinjestatus () {
+    $("#PageMainmenu").show();
+    $("#Pagelinjestatus").hide();
+
+}
+
+function Pagechangetolinjestatus () {
+    $("#PageMainmenu").hide();
+    $("#Pagelinjestatus").show();
+
+}
+
+function Pagechangetoprodplanlinje () {
+    $("#PageMainmenu").hide();
+    $("#Pageprodplanlinje").show();
+}
+
+function Pagechangetomainfromprodplanlinje () {
+    $("#PageMainmenu").show();
+    $("#Pageprodplanlinje").hide();
+}
